4f58053afa4a01c9f3ed61638f89b89ce55173ed
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-04-16 00:30:31 +0530

    Rachit Dhaka v1

--- src/component/FooterV1.jsx
+++ src/component/FooterV1.jsx
@@ -3,9 +3,10 @@ import { NavLink } from "react-router";
 const FooterV1 = () => {
   return (
     <div>
-      
       <div className="text-[10px] text-[#9b9b9b] flex justify-center  mb-2">
+        <a href="#">
         v0
+        </a>
       </div>
       <div className="text-[10px] text-[#9b9b9b] flex justify-center  mb-3">
         Made with 💖 . Rachit Dhaka
